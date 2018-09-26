2ea2ed5a6b4fb1d1b379a180c20512c37e823ec5
Richard Guan <guanr@umich.edu>
2018-09-25 23:30:39 -0400

    Adding first template

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello world"
+    return render_template("layout.html")
 
 
 if __name__ == '__main__':
